2a8491ea610f6a26b2c56f8b2a4cf05745e2dd60
YUJISANG <54089426+yujisang93@users.noreply.github.com>
2020-02-21 18:37:59 +0900

    유지상_메인

--- Js/index.js
+++ Js/index.js
@@ -2,56 +2,50 @@
 
 $(function(){
 
-
-    // $('#navOptionDiv').click(function(){ // ID가 toggleButton인 요소를 클릭하면
-	// 	var state = $('#toggleMenu').css('display'); // state 변수에 ID가 moreMenu인 요소의 display의 속성을 '대입'
-	// 	if(state == 'none'){ // state가 none 상태일경우 
-	// 		$('#toggleMenu').show("slide", { direction: "right" }, 400); // ID가 moreMenu인 요소를 show();
-	// 	}else{ // 그 외에는
-	// 		$('#toggleMenu').hide("slide", { direction: "right" }, 400); // ID가 moreMenu인 요소를 hide();			
-	// 	}
-	// });
-
-
-
-
-	$("#t1").click(function(){
-		// var offset = $('#mainDiv2').offset(); //선택한 태그의 위치를 반환
-
-        //     //animate()메서드를 이용해서 선택한 태그의 스크롤 위치를 지정해서 0.4초 동안 부드럽게 해당 위치로 이동함 
-
-		// $('html').animate({scrollTop : offset.top}, 1000);
-		
-		$("#mainDiv").stop().animate({bottom:"100%"},1000)
+	// 메인 skip 버튼
+	$("#skipBtn1").click(function(){
+		$("#mainDiv1").stop().animate({bottom:"100%"},1000)
 		$("#mainDiv2").stop().animate({top:"0"},1000,function(){
-			$('#div2_1').stop().delay(300).animate({right:"0"},500);
+			$('#loginDiv').stop().delay(300).animate({right:"0"},500);
 		})
-
-
 	})
+	// 메인 skip 버튼
 
-	$("#t3").click(function(){
-		$("#mainDiv").stop().animate({bottom:"0"},1000)
+	// 동영상 화면 이동
+	$("#upBtn").click(function(){
+		$("#mainDiv1").stop().animate({bottom:"0"},1000)
 		$("#mainDiv2").stop().animate({top:"100%"},1000)
-	})
 
-	$("#div2_1").click(function(){
-		$("#div2_1").stop().animate({right:"-30%"},500,function(){
-			$("#div2_2").stop().delay(300).animate({right:"0"},500)
-		})
-	})
+		
+		$("#loginDiv").stop().animate({right:"-30%"},1000)
+		$("#joinDiv").stop().animate({right:"-30%"},1000)
+		$("#findDiv").stop().animate({right:"-30%"},1000)
 
-	$("#div2_2").click(function(){
-		$("#div2_2").stop().animate({right:"-30%"},500,function(){
-			$("#div2_1").stop().delay(300).animate({right:"0"},500)
-		})
+
 	})
+	// 동영상 화면 이동
+
+	// $("#loginDiv").click(function(){
+	// 	$("#loginDiv").stop().animate({right:"-30%"},500,function(){
+	// 		$("#joinDiv").stop().delay(300).animate({right:"0"},500)
+	// 	})
+	// })
+
+	// $("#joinDiv").click(function(){
+	// 	$("#joinDiv").stop().animate({right:"-30%"},500,function(){
+	// 		$("#loginDiv").stop().delay(300).animate({right:"0"},500)
+	// 	})
+	// })
 
        
 
 
-
-
+	// 화면사이즈 실시간 width 리턴
+	$( window ).resize(function() {
+		var windowWidth = $( window ).width();
+		console.log(windowWidth)
+	 });
+	 // 화면사이즈 실시간 width 리턴
 
 
 

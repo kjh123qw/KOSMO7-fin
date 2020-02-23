16368fc966e037aae96b066ac4641664bb882620
YUJISANG <54089426+yujisang93@users.noreply.github.com>
2020-02-23 17:33:09 +0900

    유지상_메인

--- Js/index.js
+++ Js/index.js
@@ -2,28 +2,236 @@
 
 $(function(){
 
+	var test = 
+
 	// 메인 skip 버튼
 	$("#skipBtn1").click(function(){
-		$("#mainDiv1").stop().animate({bottom:"100%"},1000)
-		$("#mainDiv2").stop().animate({top:"0"},1000,function(){
-			$('#loginDiv').stop().delay(300).animate({right:"0"},500);
-		})
+
+		var width = returnWidth();
+
+		if(width < 576){
+			$("#mainDiv1").stop().animate({bottom:"100%"},1000)
+			$("#mainDiv2").stop().animate({top:"0"},1000,)
+
+			$("#loginDiv").css('z-index', 200);
+			$("#joinDiv").css('z-index', 100);
+			$("#findDiv").css('z-index', 100);
+		}else if(width < 768){
+			$("#mainDiv1").stop().animate({bottom:"100%"},1000)
+			$("#mainDiv2").stop().animate({top:"0"},1000,)
+
+			$("#loginDiv").css('z-index', 200);
+			$("#joinDiv").css('z-index', 100);
+			$("#findDiv").css('z-index', 100);
+		}else if(width < 992){
+			$("#mainDiv1").stop().animate({bottom:"100%"},1000)
+			$("#mainDiv2").stop().animate({top:"0"},1000,function(){
+				$('#loginDiv').stop().delay(300).animate({right:"0"},500);
+			})
+		}else if(width < 1200 ){
+			$("#mainDiv1").stop().animate({bottom:"100%"},1000)
+			$("#mainDiv2").stop().animate({top:"0"},1000,function(){
+				$('#loginDiv').stop().delay(300).animate({right:"0"},500);
+			})
+		}else{
+			$("#mainDiv1").stop().animate({bottom:"100%"},1000)
+			$("#mainDiv2").stop().animate({top:"0"},1000,function(){
+				$('#loginDiv').stop().delay(300).animate({right:"0"},500);
+			})
+		}
+
+
+
 	})
 	// 메인 skip 버튼
 
 	// 동영상 화면 이동
 	$("#upBtn").click(function(){
-		$("#mainDiv1").stop().animate({bottom:"0"},1000)
-		$("#mainDiv2").stop().animate({top:"100%"},1000)
 
-		
-		$("#loginDiv").stop().animate({right:"-30%"},1000)
-		$("#joinDiv").stop().animate({right:"-30%"},1000)
-		$("#findDiv").stop().animate({right:"-30%"},1000)
+			
 
+		var width = returnWidth();
+
+		if(width < 576){
+			$("#mainDiv1").stop().animate({bottom:"0"},1000)
+			$("#mainDiv2").stop().animate({top:"100%"},1000)
+
+			$("#loginDiv").css('z-index', 200);
+			$("#joinDiv").css('z-index', 100);
+			$("#findDiv").css('z-index', 100);
+
+			$("#loginDiv").stop().animate({right:"0"},1000)
+			$("#joinDiv").stop().animate({right:"0"},1000)
+			$("#findDiv").stop().animate({right:"0"},1000)
+		}else if(width < 768){
+			$("#mainDiv1").stop().animate({bottom:"0"},1000)
+			$("#mainDiv2").stop().animate({top:"100%"},1000)
+
+			$("#loginDiv").css('z-index', 200);
+			$("#joinDiv").css('z-index', 100);
+			$("#findDiv").css('z-index', 100);
+
+			$("#loginDiv").stop().animate({right:"0"},1000)
+			$("#joinDiv").stop().animate({right:"0"},1000)
+			$("#findDiv").stop().animate({right:"0"},1000)
+		}else if(width < 992){
+
+			$("#mainDiv1").stop().animate({bottom:"0"},1000)
+			$("#mainDiv2").stop().animate({top:"100%"},1000)
+			
+			$("#loginDiv").stop().animate({right:"-40%"},1000)
+			$("#joinDiv").stop().animate({right:"-40%"},1000)
+			$("#findDiv").stop().animate({right:"-40%"},1000)
+
+		}else if(width < 1200 ){
+
+			$("#mainDiv1").stop().animate({bottom:"0"},1000)
+			$("#mainDiv2").stop().animate({top:"100%"},1000)
+			
+			$("#loginDiv").stop().animate({right:"-40%"},1000)
+			$("#joinDiv").stop().animate({right:"-40%"},1000)
+			$("#findDiv").stop().animate({right:"-40%"},1000)
+			
+		}else{
+
+			$("#mainDiv1").stop().animate({bottom:"0"},1000)
+			$("#mainDiv2").stop().animate({top:"100%"},1000)
+			
+			$("#loginDiv").stop().animate({right:"-40%"},1000)
+			$("#joinDiv").stop().animate({right:"-40%"},1000)
+			$("#findDiv").stop().animate({right:"-40%"},1000)
+
+		}
+
+	
 
 	})
 	// 동영상 화면 이동
+
+	//회원가입, 비밀번호찾기 화면전환
+	$("#joinBtn").click(function(){
+
+		var width = returnWidth();
+
+		if(width < 576){
+
+			$("#loginDiv").css('z-index', 200);
+			$("#joinDiv").css('z-index', 100);
+			$("#findDiv").css('z-index', 100);
+
+			$("#loginDiv").stop().animate({right:"-100%"},500)
+			$("#findDiv").stop().animate({right:"-100%"},500)
+
+		}else if(width < 768){
+
+			$("#loginDiv").css('z-index', 200);
+			$("#joinDiv").css('z-index', 100);
+			$("#findDiv").css('z-index', 100);
+
+			$("#loginDiv").stop().animate({right:"-100%"},500)
+			$("#findDiv").stop().animate({right:"-100%"},500)
+
+		}else if(width < 992){
+
+			$("#loginDiv").stop().animate({right:"-40%"},500,function(){
+				$("#joinDiv").stop().delay(300).animate({right:"0"},500)
+			})
+
+		}else if(width < 1200){
+
+			$("#loginDiv").stop().animate({right:"-40%"},500,function(){
+				$("#joinDiv").stop().delay(300).animate({right:"0"},500)
+			})
+
+		}else{
+			$("#loginDiv").stop().animate({right:"-40%"},500,function(){
+				$("#joinDiv").stop().delay(300).animate({right:"0"},500)
+			})
+		}
+		
+	})
+
+	$("#findBtn").click(function(){
+
+		var width = returnWidth();
+
+		if(width < 576){
+
+			$("#loginDiv").css('z-index', 200);
+			$("#joinDiv").css('z-index', 100);
+			$("#findDiv").css('z-index', 100);
+
+			$("#loginDiv").stop().animate({right:"-100%"},500)
+			$("#joinDiv").stop().animate({right:"-100%"},500)
+
+		}else if(width < 768){
+
+			$("#loginDiv").css('z-index', 200);
+			$("#joinDiv").css('z-index', 100);
+			$("#findDiv").css('z-index', 100);
+
+			$("#loginDiv").stop().animate({right:"-100%"},500)
+			$("#joinDiv").stop().animate({right:"-100%"},500)
+
+		}else if(width < 992){
+			$("#loginDiv").stop().animate({right:"-40%"},500,function(){
+				$("#findDiv").stop().delay(300).animate({right:"0"},500)
+			})
+		}else if(width < 1200){
+			$("#loginDiv").stop().animate({right:"-40%"},500,function(){
+				$("#findDiv").stop().delay(300).animate({right:"0"},500)
+			})
+		}else{
+			$("#loginDiv").stop().animate({right:"-40%"},500,function(){
+				$("#findDiv").stop().delay(300).animate({right:"0"},500)
+			})
+		}
+	})
+
+	$(".goLogin").click(function(){
+
+		var width = returnWidth();
+
+		if(width < 576){
+
+			$("#loginDiv").css('z-index', 200);
+			$("#joinDiv").css('z-index', 100);
+			$("#findDiv").css('z-index', 100);
+
+			$("#loginDiv").stop().animate({right:"0%"},1000)
+			$("#joinDiv").stop().animate({right:"0%"},1000)
+			$("#findDiv").stop().animate({right:"0%"},1000)
+
+		}else if(width < 768){
+
+			$("#loginDiv").css('z-index', 200);
+			$("#joinDiv").css('z-index', 100);
+			$("#findDiv").css('z-index', 100);
+
+			$("#loginDiv").stop().animate({right:"0%"},1000)
+			$("#joinDiv").stop().animate({right:"0%"},1000)
+			$("#findDiv").stop().animate({right:"0%"},1000)
+
+		}else if(width < 992){
+
+			$("#joinDiv").stop().animate({right:"-40%"},500)
+			$("#findDiv").stop().animate({right:"-40%"},500,function(){
+				$("#loginDiv").stop().delay(300).animate({right:"0"},500)
+			})
+
+		}else if(width < 1200){
+			$("#joinDiv").stop().animate({right:"-40%"},500)
+			$("#findDiv").stop().animate({right:"-40%"},500,function(){
+				$("#loginDiv").stop().delay(300).animate({right:"0"},500)
+			})
+		}else{
+			$("#joinDiv").stop().animate({right:"-40%"},500)
+			$("#findDiv").stop().animate({right:"-40%"},500,function(){
+				$("#loginDiv").stop().delay(300).animate({right:"0"},500)
+			})
+		}
+	})
+	//회원가입, 비밀번호찾기 화면전환
 
 	// $("#loginDiv").click(function(){
 	// 	$("#loginDiv").stop().animate({right:"-30%"},500,function(){
@@ -40,16 +248,23 @@ $(function(){
        
 
 
-	// 화면사이즈 실시간 width 리턴
-	$( window ).resize(function() {
-		var windowWidth = $( window ).width();
-		console.log(windowWidth)
-	 });
-	 // 화면사이즈 실시간 width 리턴
-
 
 
 
 
 
 })
+
+function returnWidth(){
+	var windowWidth;
+	windowWidth = $( window ).width();
+	return windowWidth;
+}
+
+function returnWidth2(){
+	var windowWidth;
+	$( window ).resize(function() {
+		windowWidth = $( window ).width();
+	 });
+	 return windowWidth;
+}
